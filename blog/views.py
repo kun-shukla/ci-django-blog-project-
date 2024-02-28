a63ff3010be73892d7639d1f9c74e1588d7e1e46
Kunal S <kun.shukla@gmail.com>
2024-02-28 04:04:11 +0000

    Make slight changes to the project / app set up

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def my_blog(request):
     return HttpResponse("Hello, Blog!")
--- codestar/urls.py
+++ codestar/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from blog import views as index_views
-
+from django.urls import path
+from blog.views import my_blog
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('blog/', my_blog, name='blog'),
     path('admin/', admin.site.urls),
-
 ]
