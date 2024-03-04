21c0e1cf4a244e46161711e892e6a4c3e0b26ae8
Kunal S <kun.shukla@gmail.com>
2024-03-04 17:36:28 +0000

    Create Comment model

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+    Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
