23a01dea2f5feca79338c5607dca62d43b8902da
kunalshukla <kun.shukla@gmail.com>
2024-03-06 11:46:30 +0000

    Create meta classes and str methods for Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,14 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+
 class Comment(models.Model):
     post = models.ForeignKey(
     Post, on_delete=models.CASCADE, related_name="comments")
@@ -23,3 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
