f675a4361aad91c3894452a6c3cfc3f43232fe52
Kunal S <kun.shukla@gmail.com>
2024-03-07 14:16:35 +0000

    Create 'queryset' using 'Post.objects'  and its  associated methods such as '.all()', '.filter()', 'order_by()'

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,7 @@ from .models import Post
 
 class PostList(generic.ListView):
     # model = Post
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all().order_by("-created_on") #this will display all db posts in descending order (newest to oldest)
+    # queryset = Post.objects.filter(author=1)
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 
