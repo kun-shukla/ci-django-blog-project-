a8ce621ebb066635bba5fffe446c2b3c43607144
Kunal S <kun.shukla@gmail.com>
2024-03-15 12:38:27 +0000

    Add rich text editor to admin for about app content editing

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import About
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(About)
