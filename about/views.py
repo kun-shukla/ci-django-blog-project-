75956bea5cc246446ea286d65b1f3dcb58bc5a1b
Kunal S <kun.shukla@gmail.com>
2024-03-15 22:35:04 +0000

    Create about page view, template and url-patterns for app and project levels

--- about/views.py
+++ about/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
+
+def about_content(request):
+     about = About.objects.all().order_by("-updated_on").first()
+
+     return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
