9f62a53e84c8629fb0820cc17da9c73bd8271726
Kunal S <kun.shukla@gmail.com>
2024-03-16 20:09:26 +0000

    added authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -45,10 +45,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'about',
+    'blog',
+    
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -59,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
@@ -118,6 +128,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
--- codestar/urls.py
+++ codestar/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     # path('blog/', my_blog, name='blog'),
     # Alternatively - path("blog/", blog_views.my_blog, name='blog'),
-    path("about/", include("about.urls"), name="about-urls"), 
+    path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")), 
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"), 
