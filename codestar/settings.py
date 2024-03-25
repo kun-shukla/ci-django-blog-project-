3713d43f66f6006926dd9911a5c79b10b092a875
Kunal S <kun.shukla@gmail.com>
2024-03-25 06:02:44 +0000

    fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,8 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','.herokuapp.com']
 
