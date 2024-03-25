f86f9b2c8265ee9c3db3a46ad92eda3dbc87223a
Kunal S <kun.shukla@gmail.com>
2024-03-25 20:01:07 +0000

    Minor change related to static syntax in settings file

--- codestar/settings.py
+++ codestar/settings.py
@@ -145,7 +145,7 @@ messages.ERROR: 'alert-danger',
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
