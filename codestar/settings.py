6599b53f09644bac5eff6c3411d4185e0e1ee862
Kunal S <kun.shukla@gmail.com>
2024-03-26 02:06:56 +0000

    Added staticfiles folder

--- codestar/settings.py
+++ codestar/settings.py
@@ -146,8 +146,8 @@ messages.ERROR: 'alert-danger',
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
