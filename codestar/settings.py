9a84b89fea505078257d025231a58c0a49d845bc
Kunal S <kun.shukla@gmail.com>
2024-03-26 01:48:28 +0000

    change to static dir path in settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -145,8 +145,8 @@ messages.ERROR: 'alert-danger',
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'staticfiles') ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
